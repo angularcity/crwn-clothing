5accb6345ba0beaa39b3ec39c9d152e0a33629fb
Harikrishnan KK <angularcity@gmail.com>
2019-07-29 11:32:50 -0400

    adding base layouts and styles for home component

---
+++ src/home.component.jsx
@@ -0,0 +1,53 @@
+// rafce
+import React from "react";
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        {/* item1 */}
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title"> Hats </h1>
+            <span className="subtitle"> Shop now </span>
+          </div>
+        </div>
+
+        {/* item2 */}
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title"> Hats </h1>
+            <span className="subtitle"> Shop now </span>
+          </div>
+        </div>
+
+        {/* item3 */}
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title"> Hats </h1>
+            <span className="subtitle"> Shop now </span>
+          </div>
+        </div>
+
+        {/* item4 */}
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title"> Hats </h1>
+            <span className="subtitle"> Shop now </span>
+          </div>
+        </div>
+
+        {/* item5 */}
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title"> Hats </h1>
+            <span className="subtitle"> Shop now </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
